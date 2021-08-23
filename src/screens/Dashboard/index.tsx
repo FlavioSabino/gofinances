e76d90193718e1266a07a904c3e9907a03f5dc4f
FlavioSabino <s.flavio10@hotmail.com>
2021-08-23 06:52:50 -0300

    commit inicial

---
+++ src/screens/Dashboard/index.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+import { HighlightCard } from "../../components/HighlightCard";
+
+
+import {
+  Container,
+  Header,
+  UserWrapper,
+  UserInfo,
+  Photo,
+  User,
+  UserGreeting,
+  UserName,
+  Icon
+} from './styles'
+
+export function Dashboard(){
+  return (
+    <Container >
+      <Header>
+        <UserWrapper>
+        <UserInfo>
+          <Photo
+           source={{ uri: 'https://avatars.githubusercontent.com/u/35316216?v=4'}}
+            />
+          <User>
+            <UserGreeting>Olá,</UserGreeting>
+            <UserName>Flavio</UserName>
+          </User>
+        </UserInfo>
+        
+        <Icon name="power"/>
+        </UserWrapper>        
+      </Header>
+
+      <HighlightCard/>
+    </Container>
+  )
+}
+
